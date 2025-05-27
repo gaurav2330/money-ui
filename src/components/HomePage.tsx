f2f04c6a064b1348d1000b5ecbb2c38fddbe3b17
Gaurav Sharma <sgrb26@gmail.com>
2025-05-27 21:33:16 +0530

    navbar addition to homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,9 @@
+import Navbar from '../components/Navbar';
+
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
-      <p className="text-lg">This is a simple home page component.</p>
+    <div className="signup-page container-fluid vh-100 vw-100">
+      <Navbar />
     </div>
   );
 }
